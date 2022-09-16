f4f57ef61a00dfd8d67b1e932e009eb66e39ef69
HichamOujari <hichamoujari99@gmail.com>
2022-09-16 16:55:26 +0100

    asdsa

--- pages/video-player.js
+++ pages/video-player.js
@@ -1,19 +1,24 @@
-import React, { Component, useRef } from 'react';
+import React, { Component, useRef, useState } from 'react';
 import styles from '../styles/VideoPlayer.module.css'
+import ReactPlayer from 'react-player'
 
 function VideoPlayer() {
-    const videoRef = useRef(null);
     const btnPlay = useRef(null);
+    const [playing, setPlaying] = useState(false)
+
+    const playVideo = () => {
+        setPlaying(true)
+        btnPlay.current.style.display = 'none'
+    }
+
+    const onEnded = () => btnPlay.current.style.display = 'block'
 
     return (
-        <div className={styles.videoPlayer}>
-            <video onEnded={()=> btnPlay.current.style.display = 'block'} ref={videoRef} className={styles.video}>
-                <source src="/video.mp4"></source>
-            </video>
-            <button ref={btnPlay} className={styles.btn} onClick={()=>{
-               videoRef.current.play()
-               btnPlay.current.style.display = 'none'
-            }}>Play Video</button>
+        <div>
+            <div className={styles.videoWrapper}>
+                <ReactPlayer playing={playing} className={styles.videoPlayer} width={'100vw'} height={'100vh'}  url={"/video.mp4"} />
+            </div>
+            <button ref={btnPlay} className={styles.btn} onClick={playVideo}>Play Video</button>
         </div>
     );
 }
