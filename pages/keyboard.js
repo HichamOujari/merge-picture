10eb309cecc3f56daa4f907d2f0ea37d672eb083
HichamOujari <hichamoujari99@gmail.com>
2022-10-04 22:23:20 +0100

    asdsaf

---
+++ pages/keyboard.js
@@ -0,0 +1,17 @@
+import React, { Component, useRef } from 'react';
+import styles from '../styles/VideoPlayer.module.css'
+
+function Keyboard() {
+    const inputRed = useRef(null);
+
+    return (
+        <div className={styles.videoPlayer}>
+            <input ref={inputRed} type="text" hidden={true} />
+            <button className={styles.btn} onClick={()=>{
+               inputRed.current.focus()
+            }}>Show keyboard</button>
+        </div>
+    );
+}
+
+export default Keyboard;
