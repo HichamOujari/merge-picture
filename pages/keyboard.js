1060e50d85761186a0bd49ba81cb5c033a74d39e
HichamOujari <hichamoujari99@gmail.com>
2022-10-05 09:53:03 +0100

    sadasd

--- pages/keyboard.js
+++ pages/keyboard.js
@@ -6,7 +6,7 @@ function Keyboard() {
 
     return (
         <div className={styles.videoPlayer}>
-            <input ref={inputRed} type="text" hidden={false} />
+            <input ref={inputRed} type="text" style={{visibility: 'hidden'}}/>
             <button className={styles.btn} onClick={()=>{
                inputRed.current.focus()
             }}>Show keyboard</button>
