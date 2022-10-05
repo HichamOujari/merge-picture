e8b864f85be394636f7f5bdd8a41937ba99d9a81
HichamOujari <hichamoujari99@gmail.com>
2022-10-05 09:59:02 +0100

    sadasd

--- pages/keyboard.js
+++ pages/keyboard.js
@@ -10,7 +10,12 @@ function Keyboard() {
                 <input ref={inputRed} type="text" />
             </div>
             <button className={styles.btn} onClick={() => {
-                inputRed.current.focus()
+                document.addEventListener('keydown', (event) => {
+                    var name = event.key;
+                    var code = event.code;
+                    // Alert the key name and key code on keydown
+                    alert(`Key pressed ${name} \r\n Key code value: ${code}`);
+                  }, false);
             }}>Show keyboard</button>
         </div>
     );
